1f12f94df493480cf7674d1662f013b1d03d1c27
Nairouz <nairouzmahmoud214@gmail.com>
2024-02-07 13:05:19 +0200

    edits2

--- Jenkins10/Controllers/CoursesController.cs
+++ Jenkins10/Controllers/CoursesController.cs
@@ -13,10 +13,21 @@ namespace Jenkins10.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Index()
+     /*   public async Task<IActionResult> Index()
         {
             var courses = await _context.Courses.ToListAsync();
             return View(courses);
+        }
+     */
+        public async Task<IActionResult> Index()
+        {
+            var courses = await getAllCourses(); // Await the asynchronous method
+            return View(courses);
+        }
+        public async Task<List<Jenkins10.Models.Course>> getAllCourses()
+        {
+            var courses = await _context.Courses.ToListAsync();
+            return courses;
         }
     }
 }
